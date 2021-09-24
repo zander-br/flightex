e4842feb3ac5e83dac2fe600bec287207772cf3d
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-09-24 06:05:53 -0300

    feat: add defdelegate generate report

--- lib/flightex.ex
+++ lib/flightex.ex
@@ -1,6 +1,7 @@
 defmodule Flightex do
   alias Flightex.Bookings.Agent, as: BookingAgent
   alias Flightex.Bookings.CreateOrUpdate, as: CreateOrUpdateBooking
+  alias Flightex.Bookings.Report, as: BookingReport
   alias Flightex.Users.Agent, as: UserAgent
   alias Flightex.Users.CreateOrUpdate, as: CreateOrUpdateUser
 
@@ -12,4 +13,5 @@ defmodule Flightex do
   defdelegate create_user(parans), to: CreateOrUpdateUser, as: :call
   defdelegate create_booking(params), to: CreateOrUpdateBooking, as: :call
   defdelegate get_booking(booking_id), to: BookingAgent, as: :get
+  defdelegate generate_report(from_date, to_date), to: BookingReport, as: :generate
 end
